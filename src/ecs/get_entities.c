6c18b6f68f6f32bc9eda0152e12fa82162df0731
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2024-04-24 10:25:18 +0200

    [ADD] ecs folder

---
+++ src/ecs/get_entities.c
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2024
+** RPG
+** File description:
+** Get entities from the system
+*/
+
+#include "rpg.h"
---
+++ src/linked_list.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** RPG
+** File description:
+** Functions related to linked_list
+*/
+
+#include "rpg.h"
+#incude "struct.h"
+
+e_list_t *push_to_list(e_list_t )
